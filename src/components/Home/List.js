897d7295615dd9ab7159f7f9d319b4af40a576f0
Jason <chenpanyong123@gmail.com>
2018-12-05 18:27:22 -0800

    Update List.js

--- src/components/Home/List.js
+++ src/components/Home/List.js
@@ -1,48 +1,104 @@
-import React from "react";
+import React, { Component }  from "react";
 import image from "../data/image.js";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
-const List = props => {
-  return (
-    <li className="list">
-      {!props.data.avatar ? (
-        <img src={image} className="li-avatar" />
-      ) : (
-        <img src={props.data.avatar} className="li-avatar" />
-      )}
-      <div className="li-mid">
-        <Link to={`/detail/${props.data._id}`}>
-          <div className="li-name">
-            {props.data.firstName} {props.data.lastName}
-          </div>
-        </Link>
+class List extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {edit:false};
+    this.clickReports = this.clickReports.bind(this);
+    this.clickManager = this.clickManager.bind(this);
+  }
+
+  deleteHandler = () => {
+    this.props.dispatch(actions.deleteEmployee(this.props.data._id));
+  }
+
+  clickReports(){
+    if (this.props.click_reportsfn){
+      this.props.click_reportsfn(this.props.data._id);
+    }
+  }
+  clickManager(){
+    if (this.props.click_managerfn){
+      console.log(this.props.data.manager);
+      if (this.props.data && this.props.data.manager) {
+        this.props.click_managerfn(this.props.data.manager);
+      }
+    }
+  }
+  render() {
+    let manager = this.props.employee.employee.filter(manager=>manager._id===this.props.data.manager)[0];
+    return (
+      <li className="list">
+        {!this.props.data.avatar ? (
+          <img src={image} className="li-avatar" />
+        ) : (
+          <img src={this.props.data.avatar} className="li-avatar" />
+        )}
+        <div className="li-mid">
+            <div className="li-name">
+              {this.props.data.firstName} {this.props.data.lastName}
+            </div>
+        </div>
+        <div className="li-gender-wrap">
+          <div className="li-gender">{this.props.data.gender}</div>
         </div>
         <div className="li-age-wrap">
-        <div className="li-age">{props.data.age}</div>
+          <div className="li-age">{this.props.data.age}</div>
         </div>
         <div className="li-title-wrap">
-        <div className="li-title">{props.data.title}</div>
+          <div className="li-title">{this.props.data.title}</div>
         </div>
         <div className="li-phone-wrap">
-        <div className="li-phone">{props.data.cellPhone}</div>
-       <a href={`tel:${props.data.cellPhone}`}>
-            <i class="fas fa-phone-volume"></i>
-              </a>
+          <div className="li-phone">{this.props.data.cellPhone}</div>
+          <a href={`tel:${this.props.data.cellPhone}`}>
+              <i className="fas fa-phone-volume"></i>
+          </a>
+        </div>
+        <div className="li-email-wrap">
+          <div className="li-email">{this.props.data.email} </div>
+          <a href={`mailto:${this.props.data.email}`}>
+            <i className="fas fa-envelope-open"></i>
+          </a>
+        </div>
+
+        <div className="li-manager-wrap">
+          <div className="li-manager">
+            <a onClick={this.clickManager}>
+              <h4>
+                { manager
+                  &&manager.firstName}
+              </h4>
+            </a>
+        </div> 
+        </div> 
+
+        <div className="li-reports-wrap">
+        <div className="li-reports">
+          <button type="button" className="btn" onClick={this.clickReports}>{this.props.data.directReports.length}</button>
+        </div>
       </div>
-      <div className="li-email-wrap">
-      <div className="li-email">{props.data.email}</div>
-      <a href={`mailto:${props.data.email}`}>
-             <i class="fas fa-envelope-open"></i>
-              </a>
+      <div className="li-actions">
+        <Link to={`/edit/${this.props.data._id}`}><button type="button" className="btn btn-primary">Edit</button></Link>
+        </div>
+        <div className="li-actions2">
+        <button type="button" className="btn btn-danger" onClick={this.deleteHandler}>Delete</button>
+        
+        
       </div>
-      <div className="li-reports-wrap">
-      <div className="li-reports">{props.data.directReports.length}</div>
-      </div>
-      <Link to={`/detail/${props.data._id}`}>
-        <i className="fas fa-angle-right" id="li-arrow" />
-      </Link>
     </li>
-  );
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    employee: state.employee,
+  };
 };
 
-export default List;
+export default connect(mapStateToProps)(List);
+
